fc69fb0a052e2b9284cd14fa28c41f3ec32c2357
Iruma Shibuya <programiruma@gmail.com>
2012-11-12 04:12:38 -0500

    Update index to static home page

--- config/routes.rb
+++ config/routes.rb
@@ -53,9 +53,9 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
-  # See how all your routes lay out with "rake routes"
+  # home how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
