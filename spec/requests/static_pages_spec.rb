e8884bd9e70204014e423fc8e3db08c6d6df99d6
Iruma Shibuya <programiruma@gmail.com>
2012-11-12 03:23:37 -0500

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      page.should have_selector('h1', text: 'Sample App')
+  end
+    it "should have the content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      page.should have_selector('title', text: "Sample App | Home")
+    end
+  end
+  describe "Help page" do
+  	it "should have the content 'Help'" do
+  	  visit '/static_pages/help'
+  	  page.should have_selector('h1', text: 'Help')
+  	end
+  	it "should have the content 'Help'" do
+  	  visit '/static_pages/help'
+  	  page.should have_selector('title', text: "Sample App | Help")
+  	end
+  end
+  describe "About page" do
+  	it "should have the content 'About us'" do
+  	  visit '/static_pages/about'
+  	  page.should have_selector('h1', text: 'About Us')
+  	end
+  	it "should have the content 'About us'" do
+  	  visit '/static_pages/about'
+  	  page.should have_selector('title', text: "Sample App | About Us")
+  	end
+  end
+end
