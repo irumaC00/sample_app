564e89a64563ab1f43cf48316096e3a7bbc966b2
Iruma Shibuya <programiruma@gmail.com>
2012-11-14 17:15:41 -0500

    Created a dynamic title that can be set or put to default.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,16 @@ describe "Static pages" do
     it "should have the h1 'Home'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_selector('h1', text: 'Home')
+      page.should have_selector('h1', text: 'Sample App')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_selector('title', text: "#{t}Home")
+      page.should have_selector('title', text: "Sample App")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', text: '#{t}Home')
     end
   end
 
