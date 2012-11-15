8f12a4bd6702edda7817967336b489ac8d7e8d7a
Iruma Shibuya <programiruma@gmail.com>
2012-11-15 04:21:41 -0500

    Installed Bootstrap, Learned how to use SCSS, Updated routes, and updated the User Interface for the Application. It finally looks like something!

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,71 +2,35 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:t) {'Sample App | '}
-
-  describe "Index page" do
-    it "should have the h1 'Index Page'" do
-      visit '/static_pages/index'
-      page.should have_selector('h1', text: 'Index Page')
-    end
-    it "should have the base title" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/index'
-      page.should have_selector('title', text: "Sample App")
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/index'
-      page.should_not have_selector('title', text: '#{t}Index')
-    end
-  end
+  subject { page }
+  let(:base_title) { "Sample App" }
 
   describe "Home page" do
-    it "should have the h1 'Home'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_selector('h1', text: 'Sample App')
-    end
-    it "should have the base title" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_selector('title', text: "Sample App")
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', text: '#{t}Home')
-    end
+    before { visit root_path }
+
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: 'Home') }
   end
 
   describe "Help page" do
-  	it "should have the h1 'Sample App | Help'" do
-  	  visit '/static_pages/help'
-  	  page.should have_selector('h1', text: 'Help')
-  	end
-  	it "should have the title 'Help'" do
-  	  visit '/static_pages/help'
-  	  page.should have_selector('title', text: "#{t}Help")
-  	end
+    before { visit help_path }
+
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
-  	it "should have the h1 'Sample App | About us'" do
-  	  visit '/static_pages/about'
-  	  page.should have_selector('h1', text: 'About Us')
-  	end
-  	it "should have the title 'About us'" do
-  	  visit '/static_pages/about'
-  	  page.should have_selector('title', text: "#{t}About Us")
-  	end
+    before { visit about_path }
+
+    it { should have_selector('h1', text: 'About Us') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact page" do
-  	it "should have the h1 'Contact'" do
-  	  visit '/static_pages/contact'
-  	  page.should have_selector('h1', text: 'Contact')
-  	end
-  	it "should have the title 'Sample App | Contact'" do
-  	  visit '/static_pages/contact'
-  	  page.should have_selector('title', text: "#{t}Contact")
-  	end
+    before { visit contact_path }
+
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
